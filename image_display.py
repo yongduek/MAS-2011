3c3f5d37d9b7563007b1d20100ac36745e1e276d
Yongduek Seo <yndk@sogang.ac.kr>
2022-09-20 11:12:32 +0900

    1

--- image_display.py
+++ image_display.py
@@ -15,9 +15,30 @@ if __name__ == "__main__":
         sys.exit(1)
     
     im = cv2.imread(imagefile)
-    
+    s = im.shape
+    print(s)
+    # cv2.imshow("window", im)
+
+    # cv2.waitKey(0)
+
     while True:
+        x = np.random.randint(0, s[1]-10)
+        y = np.random.randint(0, s[0]-10)
+
+        color = np.random.randint(0, 256, size=3)
+        # print(color)
+
+        im[y:y+10, x:x+10] = color 
+
+        # im[y:y+10, x:x+10, 0] = color[0] 
+        # im[y:y+10, x:x+10, 1] = color[1]
+        # im[y:y+10, x:x+10, 2] = color[2]
+
         cv2.imshow("dispWindow", im)
         
-        ch = cv2.waitKey(10)  # press ESC to stop
+        ch = cv2.waitKey(1)  # press ESC to stop
         if ch == 27: break
+
+
+
+#
