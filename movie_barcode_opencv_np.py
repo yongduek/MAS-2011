d53e04dc8f3274f22e44f2aa982e99cf9ee192e5
Yongduek <yongduek.seo@gmail.com>
2022-10-17 09:52:48 +0900

    movie

--- movie_barcode_opencv_np.py
+++ movie_barcode_opencv_np.py
@@ -6,58 +6,62 @@ import os
 from pathlib import Path
 import cv2 # opencv 
 
-cwd = os.path.dirname(__file__)
-print('cwd: ', cwd)
+def main():
+    cwd = os.path.dirname(__file__)
+    print('cwd: ', cwd)
 
-filename = Path(cwd) / 'pixels/time-lapse-video-of-sunset-by-the-sea-854400.mp4'
-print(filename)
+    filename = Path(cwd) / 'pixels/time-lapse-video-of-sunset-by-the-sea-854400.mp4'
+    print(filename)
 
-reader = cv2.VideoCapture(str(filename))
+    video = cv2.VideoCapture(str(filename))
 
-width = int(reader.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = reader.get(cv2.CAP_PROP_FPS)  # this may be float such as 29.97
-nframes = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
-fourcc = int(reader.get(cv2.CAP_PROP_FOURCC))
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = video.get(cv2.CAP_PROP_FPS)  # this may be float such as 29.97
+    nframes = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fourcc = int(video.get(cv2.CAP_PROP_FOURCC))
 
-print(width, height, fps, nframes, fourcc)
+    print(width, height, fps, nframes, fourcc)
 
-columns = np.zeros((height, nframes, 3), dtype='uint8')
-i = 0
-while reader.isOpened():
-    ret, frame = reader.read() 
-    if not ret:
-        break
+    columns = np.zeros((height, nframes, 3), dtype='uint8')
+    i = 0
+    while video.isOpened():
+        ret, frame = video.read() 
+        if ret == False:
+            break
 
-    array = frame # just rename, nothing
+        array = frame # just rename, nothing
 
-    # Collapse down to a column.
-    column = array.mean(axis=1)  # (height, 3)
-    if i < 3: print(column.shape)
+        # Collapse down to a column.
+        # column = array.mean(axis=1)  # (height, 3)
+        # Convert to bytes, as the `mean` turned our array into floats.
+        # column = column.clip(0, 255).astype('uint8')
 
-    # Convert to bytes, as the `mean` turned our array into floats.
-    column = column.clip(0, 255).astype('uint8')
+        column = array[:, array.shape[1]//2]
 
-    if i < 3: 
-        print(i, frame.shape, type(frame), column.shape)
+        if i < 3: 
+            print(i, frame.shape, type(frame), column.shape)
 
-    columns[:,i,:] = column
+        columns[:,i,:] = column
 
-    i += 1
+        i += 1
 
-    # cv2.imshow('frameWindow', array)
-    # if cv2.waitKey(1) == 27:  # ESC
-    #     break
-#
-print(f'finished processing {i} frames ({nframes})')
+        cv2.imshow('frameWindow', array)
+        if cv2.waitKey(1) == 27:  # ESC
+            break
+    #
+    print(f'finished processing {i} frames ({nframes})')
 
-full_array = columns 
-print(len(columns), full_array.shape)
+    full_array = columns 
+    print(len(columns), full_array.shape)
 
-full_array = cv2.resize(full_array, (800,200))
-print(full_array.dtype)
+    full_array = cv2.resize(full_array, (800,200)) # make a smaller version
+    print(full_array.dtype)
 
-imageio.imwrite('barcode_cv2_numpy.jpg', full_array)
+    imageio.imwrite('barcode_cv2_numpy.jpg', full_array)
 
-cv2.imshow('frameWindow', full_array)
-cv2.waitKey(0)
+    cv2.imshow('frameWindow', full_array)
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
